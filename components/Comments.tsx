962f82c44ee2f1efc4e1f5b762a1c4c89eb6ee50
sakana <102203628+SQ-Sakana@users.noreply.github.com>
2022-05-14 00:10:17 +0800

    Update Comments.tsx

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -4,10 +4,10 @@ const Comments = () => {
   return (
     <div id="comments-section" className="border rounded border-gray-400/30 mt-4 p-4 md:-mx-4">
       <Giscus
-        repo="username/repoid"
-        repoId=""
-        category="giscus"
-        categoryId=""
+        repo="SQ-Sakana/sakana-blog"
+        repoId="R_kgDOHVK3Fg"
+        category="General"
+        categoryId="DIC_kwDOHVK3Fs4CPFpx"
         mapping="pathname"
         reactionsEnabled="1"
         theme="preferred_color_scheme"
